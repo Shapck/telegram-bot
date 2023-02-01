964950e1744258ea175eacce32751e2ac362020a
Дима <dimonsoldatov@gmail.com>
2023-02-01 23:30:09 +0300

    dz

--- src/main/java/pro/sky/telegrambot/component/NotificationTaskTimer.java
+++ src/main/java/pro/sky/telegrambot/component/NotificationTaskTimer.java
@@ -1,13 +1,14 @@
 package pro.sky.telegrambot.component;
 
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import pro.sky.telegrambot.repository.NotificationRepository;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeUnit;
-
+@Component
 public class NotificationTaskTimer {
     private final NotificationRepository notificationRepository;
     private final SendHelp sendHelp;
